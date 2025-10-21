95ef5b31b0537681ad710e40cd679f1839a50afa
Saqeeb-Khan-Repo <khansaqeeb332@gmail.com>
2025-10-21 08:52:47 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Get the base path from the homepage property in package.json
-const base = "/";
+const base = "/myntra-react/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
