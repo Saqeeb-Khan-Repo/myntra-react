3a80a6dc7f377121c4cdcdc33099ed58f216a50d
Saqeeb-Khan-Repo <khansaqeeb332@gmail.com>
2025-10-21 08:59:52 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Get the base path from the homepage property in package.json
-const base = "/myntra-react/";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // 🔑 Crucial step for GitHub Pages subdirectories
-  base: base,
+  base: "/myntra-react/",
 });
